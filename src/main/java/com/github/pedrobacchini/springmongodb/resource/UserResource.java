b990146d8e68312600e759b84400e281f54b34ef
Pedro Bacchini <pedrobacchini@outlook.com>
2019-06-14 09:38:30 -0300

    Atualização do usuario com PUT

--- src/main/java/com/github/pedrobacchini/springmongodb/resource/UserResource.java
+++ src/main/java/com/github/pedrobacchini/springmongodb/resource/UserResource.java
@@ -45,4 +45,12 @@ public class UserResource {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+        User user = userService.fromDTO(userDTO);
+        user.setId(id);
+        userService.update(user);
+        return ResponseEntity.noContent().build();
+    }
 }
